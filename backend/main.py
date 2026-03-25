44ac7fb5ce2dc8032ddb8e1cced461c43869a479
Joba Amos <ojb.amos@gmail.com>
2026-03-25 19:56:00 +0100

    Fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CampusLink API", version="1.0.0")
 
+origins = [
+    "https://campuslink-app.netlify.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "*"
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
